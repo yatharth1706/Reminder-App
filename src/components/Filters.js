974a7ac1a4186066a1c493e8b3286a8c528628e6
yatharth <yatharthverma070@gmail.com>
2022-10-05 10:14:38 +0530

    Add filters basic ui layout

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -1,7 +1,21 @@
 import React from "react";
+import { Button, Input } from "reactstrap";
+import reminderImage from "../images/icons/reminder.png";
 
 function Filters() {
-  return <div>Filters</div>;
+  return (
+    <div className="flex justify-between">
+      <div className="flex space-x-6">
+        <Input type="search" placeholder="Search any reminder ..." style={{ width: "200px" }} />
+        <Input type="datetime-local" style={{ width: "200px" }} />
+      </div>
+      <div>
+        <Button color="primary" className="flex">
+          Create Reminder
+        </Button>
+      </div>
+    </div>
+  );
 }
 
 export default Filters;
--- src/components/LeftSideBar.js
+++ src/components/LeftSideBar.js
@@ -7,19 +7,19 @@ import Calender from "../images/icons/Calender.svg";
 function LeftSideBar() {
   return (
     <div className="bg-white drop-shadow-2xl rounded-md w-1/6 h-96 flex flex-col space-y-3 px-3 py-6">
-      <span className="flex space-x-2 items-center bg-blue-600 rounded-sm px-4 py-2 text-white">
+      <span className="flex space-x-2 items-center bg-blue-600 rounded-sm px-3 py-2 text-white">
         <img src={All} className="w-4" />
         <span>All</span>
       </span>
-      <span className="flex space-x-2 items-center px-4 py-2">
+      <span className="flex space-x-2 items-center px-3 py-2">
         <img src={Pending} className="w-4" />
         <span>Pending</span>
       </span>
-      <span className="flex space-x-2 items-center px-4 py-2">
+      <span className="flex space-x-2 items-center px-3 py-2">
         <img src={Completed} className="w-4 fill-cyan-500 stroke-zinc-200" />
         <span>Completed</span>
       </span>
-      <span className="flex space-x-2 items-center px-4 py-2">
+      <span className="flex space-x-2 items-center px-3 py-2">
         <img src={Calender} className="w-4 stroke-zinc-200" />
         <span>Calender</span>
       </span>
