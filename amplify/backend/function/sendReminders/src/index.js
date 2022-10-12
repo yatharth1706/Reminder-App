9e66b51ce5587628f5e4f0a6fb9ecf0f4d5fd8c2
yatharth <yatharthverma070@gmail.com>
2022-10-12 20:28:24 +0530

    trying something with lambda fn

--- amplify/backend/function/sendReminders/src/index.js
+++ amplify/backend/function/sendReminders/src/index.js
@@ -1,6 +1,19 @@
 /**
  * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
  */
+
+const { DataStore } = require("aws-amplify");
+
+const getReminders = async () => {
+  try {
+    const models = await DataStore.query(Reminders);
+
+    console.log(models);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
 exports.handler = async (event) => {
-  console.log("Lambda is working");
+  getReminders();
 };
